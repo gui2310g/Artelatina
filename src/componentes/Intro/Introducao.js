0781134a6c7c87334e6a9381e11ca7a2591ce22b
Guilherme Fontes <113401914+gui2310g@users.noreply.github.com>
2023-07-10 20:34:36 -0300

    Update Introducao.js

--- src/componentes/Intro/Introducao.js
+++ src/componentes/Intro/Introducao.js
@@ -6,7 +6,7 @@ const Introducao = () => {
 
             <h1>Arte latina</h1>
 
-            <div class='apresentacao'>
+            <div className='apresentacao'>
                
                 <p id='texto'>Atuando na Serra Gaúcha, com habilidades tradicionais e criatividade, a Arte latina oferece produtos artesanais únicos, como amigurumis, mosaico, feltro e artesanato em geral. Além disso, Arte latina também oferece serviços personalizados para eventos especiais. Sua paixão pela arte e artesanato e compromisso com a qualidade tornam suas criações autênticas e encantadoras.</p>
                              
